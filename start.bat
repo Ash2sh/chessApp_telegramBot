b80c22463d30e591d72800e1ac87de450d2186e3
Adil <111128826+Ash2sh@users.noreply.github.com>
2025-04-22 10:46:23 +0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,7 @@
 @echo off
-for /f "tokens=1,* delims==" %%A in (.env) do set %%A=%%B
+for /f "usebackq tokens=1,* delims==" %%A in (".env") do (
+    set "value=%%B"
+    call set %%A=%%value:"=%%
+)
 python -m bot
 pause
